59511a27561f17b4dfec7501a1a6725a37565415
Ken Henkel <KenHenkel95@gmail.com>
2019-01-22 00:40:16 -0500

    Added more fuctions to the math test

--- Gomath.go
+++ Gomath.go
@@ -5,7 +5,23 @@ import "fmt"
 func add(x int, y int) int{
 	return x + y
 }
+func subtract(x int, y int) int{
+	return x - y
+}
+func divide(x int, y int) int{
+	return x / y
+}
+func multiply(x int, y int) int{
+	return x * y
+}
+func modulus(x int, y int) int{
+	return x % y
+}
 
 func main(){
 	fmt.Println(add(42,13))
+	fmt.Println(subtract(42,13))
+	fmt.Println(divide(42,13))
+	fmt.Println(multiply(42,13))
+	fmt.Println(modulus(42,13))
 }
